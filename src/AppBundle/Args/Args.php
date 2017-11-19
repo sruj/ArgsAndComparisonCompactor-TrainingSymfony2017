eeeb085df2057c61500447c1aba35ad588d7e46f
sruj <chryplewiczpawel@wp.pl>
2017-11-19 08:18:29 +0100

    Added Module rules.

--- src/AppBundle/Args/Args.php
+++ src/AppBundle/Args/Args.php
@@ -9,6 +9,23 @@ class Args
     private $pieces;
     private $commandPieces;
 
+    /**
+     * Zadania tej klasy:
+     * - rozpoznać schemat formatki (np "l,p#,d*" oznacza: " " bool, "#" int, "*" string)
+     * - rozdzielić komendę na części (tablica argumentów i ich wartości)
+     * - sprawdzić poprawność typów wprowadzonych wartości argumentów komendy na podstawie formatki
+     * - zwrócić wartości argumentów komendy we właściwym typie danych
+     * - zwracać błędy
+     *
+     * Typy Danych:
+     * - " " bool,
+     * - "#" int,
+     * - "*" string
+     *
+     * Kontrola Błędów:
+     * - gdy wartość argumentu komendy pusta
+     * - gdy ma niewłaściwy typ danych
+     **/
     public function __construct($schema, $command)
     {
         $schema = "l,p#,d*";
